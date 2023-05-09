ce4bffa33b44d38a110b682683b90cd4d5cc1cec
Code-Nature <codenature01001@gmail.com>
2023-05-08 20:17:10 -0500

    base de la aplicacion de Todos

---
+++ tasks/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.task_list, name='task_list'),
+    path('new/', views.task_new, name='task_new'),
+    path('edit/<int:pk>/', views.task_edit, name='task_edit'),
+    path('delete/<int:pk>/', views.task_delete, name='task_delete'),
+]
---
+++ tasks/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Task
+# from forms import TaskForm
+from .forms import TaskForm
+
+# Create your views here.
+
+def task_list(request):
+    tasks = Task.objects.order_by('due_date', 'priority')
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+def task_new(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/task_new.html', {'form': form})
+
+def task_edit(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'tasks/task_edit.html', {'form': form})
+
+def task_delete(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'tasks/task_confirm_delete.html', {'task': task})
