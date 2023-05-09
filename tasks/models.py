323ac65a496f558cdc3cf5018d337630a73072dd
Code-Nature <codenature01001@gmail.com>
2023-05-09 07:49:25 -0500

    mejorando la legibilidad del codigo del archivo models

--- tasks/models.py
+++ tasks/models.py
@@ -3,10 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Task(models.Model):
+    PRIORITY_CHOICES = (
+        ('high', 'High'),
+        ('medium', 'Medium'),
+        ('low', 'Low'),
+    )
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     due_date = models.DateField()
-    priority = models.CharField(max_length=200, choices=[('high', 'High'), ('medium', 'Medium'), ('low', 'Low')])
+    priority = models.CharField(max_length=200, choices=PRIORITY_CHOICES)
     completed = models.BooleanField(default=False)
 
     def __str__(self):
