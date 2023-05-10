5f76927166027b83a712eebf843d46a63c9e7443
Code-Nature <codenature01001@gmail.com>
2023-05-09 19:59:38 -0500

    se mejoro el diseño de los templates de la app tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('new/', views.task_new, name='task_new'),
     path('edit/<int:pk>/', views.task_edit, name='task_edit'),
     path('delete/<int:pk>/', views.task_delete, name='task_delete'),
+    path('completed/<int:pk>/', views.task_completed, name='task_completed'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
 from .models import Task
-# from forms import TaskForm
-from .forms import TaskForm
 
 # Create your views here.
 
@@ -11,28 +10,62 @@ def task_list(request):
 
 def task_new(request):
     if request.method == "POST":
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('task_list')
-    else:
-        form = TaskForm()
-    return render(request, 'tasks/task_new.html', {'form': form})
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        due_date = request.POST.get('due_date')
+        priority = request.POST.get('priority')
+
+        task = Task(
+            title=title,
+            description=description,
+            due_date=due_date,
+            priority=priority,
+        )
+        task.save()
+        return HttpResponseRedirect('/tasks/')
+    
+    return render(request, 'tasks/task_new.html')
 
 def task_edit(request, pk):
     task = get_object_or_404(Task, pk=pk)
     if request.method == "POST":
-        form = TaskForm(request.POST, instance=task)
-        if form.is_valid():
-            form.save()
-            return redirect('task_list')
-    else:
-        form = TaskForm(instance=task)
-    return render(request, 'tasks/task_edit.html', {'form': form})
+        title = request.POST['title']
+        description = request.POST['description']
+        due_date = request.POST['due_date']
+        priority = request.POST['priority']
+        # completed = request.POST['completed']
+
+        task.title = title
+        task.description = description
+        task.due_date = due_date
+        task.priority = priority
+        # task.completed = completed
+        task.save()
+
+        return HttpResponseRedirect('/tasks/')
+    
+    context = {
+        'task': task
+    }
+    return render(request, 'tasks/task_edit.html', context)
+
+def task_completed(request, pk):
+    task = Task.objects.filter(completed=True).order_by('due_date', 'priority')
+    context = {
+        'task': task
+    }
+    return render(request, 'tasks/task_completed.html', context)
+
 
 def task_delete(request, pk):
+
     task = get_object_or_404(Task, pk=pk)
     if request.method == "POST":
         task.delete()
-        return redirect('task_list')
-    return render(request, 'tasks/task_confirm_delete.html', {'task': task})
+        return HttpResponseRedirect('/tasks/')
+    
+    context = {
+        'task': task
+    }
+    
+    return render(request, 'tasks/task_delete.html', context)
